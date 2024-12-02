1f89f494b64ca78f3f892ad67a338e5b4f687d07
Anton Pogorelov <a.pogorelov@biglion.ru>
2024-12-02 10:49:12 +0300

    успешный урок по unittest

--- test_script_unittest.py
+++ test_script_unittest.py
@@ -2,43 +2,37 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import unittest
 
-class Test_class_name(unittest.TestCase):
+class TestClassName(unittest.TestCase):
     def test_1url(self):
         browser = webdriver.Chrome()
         browser.get('http://suninjuly.github.io/registration1.html')
 
-        input1 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your first name"]')
-        input1.send_keys("Ivan")
-        input2 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your last name"]')
-        input2.send_keys("Sysan")
-        input3 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your email"]')
-        input3.send_keys("xz")
-        input4 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your phone"]')
-        input4.send_keys("123")
-        input5 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your address"]')
-        input5.send_keys("RUS")
+        First_name = browser.find_element(By.XPATH, '// input[@placeholder="Input your first name"]')
+        First_name.send_keys("1111")
+        Last_name = browser.find_element(By.XPATH, '//input[@placeholder="Input your last name"]')
+        Last_name.send_keys("2222")
+        Email = browser.find_element(By.XPATH, '//input[@placeholder="Input your email"]')
+        Email.send_keys("3333")
         button = browser.find_element(By.CSS_SELECTOR, "button.btn")
         button.click()
 
-        self.assertEqual('successfully' in browser.find_element(By.CSS_SELECTOR, ".container").text)
+        self.assertEqual('Congratulations! You have successfully registered!', browser.find_element(By.TAG_NAME, 'h1').text)
 
 
     def test_2url(self):
         browser = webdriver.Chrome()
         browser.get('http://suninjuly.github.io/registration2.html')
 
-        input1 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your first name"]')
-        input1.send_keys("Ivan")
-        input3 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your address"]')
-        input3.send_keys("address")
-        input4 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your phone:"]')
-        input4.send_keys("123")
-        input5 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your address:"]')
-        input5.send_keys("RUS")
+        First_name = browser.find_element(By.XPATH, '// input[@placeholder="Input your first name"]')
+        First_name.send_keys("1111")
+        Last_name = browser.find_element(By.XPATH, '//input[@placeholder="Input your last name"]')
+        Last_name.send_keys("2222")
+        Email = browser.find_element(By.XPATH, '//input[@placeholder="Input your email"]')
+        Email.send_keys("3333")
         button = browser.find_element(By.CSS_SELECTOR, "button.btn")
         button.click()
 
-        self.assertEqual('successfully' in browser.find_element(By.CSS_SELECTOR, ".container").text)
+        self.assertEqual('Congratulations! You have successfully registered!', browser.find_element(By.TAG_NAME, 'h1').text)
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
         unittest.main()
