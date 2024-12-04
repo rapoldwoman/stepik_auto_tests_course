b15a4dc31ee8ee05931176d7fc9faec58623f096
Anton Pogorelov <a.pogorelov@biglion.ru>
2024-12-04 17:03:37 +0300

    фикс

--- test_fixture_autouse.py
+++ test_fixture_autouse.py
@@ -18,8 +18,8 @@ def prepare_data():
 
 class TestMainPage1():
     def test_guest_should_see_login_link(self, browser):
-    browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
 
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
