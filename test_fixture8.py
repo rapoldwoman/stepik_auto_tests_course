0fe1e07a9130f6cd5d882c9c59af31001cbdef90
Anton Pogorelov <a.pogorelov@biglion.ru>
2024-12-07 06:56:54 +0300

    фикс

--- test_fixture8.py
+++ test_fixture8.py
@@ -15,12 +15,17 @@ def browser():
 
 class TestMainPage1():
 
-    @pytest.mark.smoke
+
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
-    @pytest.mark.regression
+
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+    @pytest.mark.xfail
+    def test_guest_should_see_search_button_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_elements(By.CSS_SELECTOR, "button.favorite")
