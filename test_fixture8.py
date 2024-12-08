d161f43230985f7c586f1394253df15afd8f91e2
Anton Pogorelov <a.pogorelov@biglion.ru>
2024-12-08 06:01:26 +0300

    фикс

--- test_fixture8.py
+++ test_fixture8.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
