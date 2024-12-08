9816d5e0439af0ba4cb92e3d338c72d815ef69de
Anton Pogorelov <a.pogorelov@biglion.ru>
2024-12-08 06:03:32 +0300

    test_guest_should_see_basket_link_on_the_main_page

--- test_fixture8.py
+++ test_fixture8.py
@@ -19,7 +19,7 @@ class TestMainPage1():
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
-    def test_guest_should_see_login_link(self, browser):
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
 
