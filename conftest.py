5093dfe8d103c113fd7540dcf53aa94cc630c652
Anton Pogorelov <a.pogorelov@biglion.ru>
2024-12-09 19:45:55 +0300

    новая фикстура для обработки данных в командной строке

--- conftest.py
+++ conftest.py
@@ -3,10 +3,24 @@ from selenium import webdriver
 import time
 import math
 
+from selenium.webdriver.support.expected_conditions import none_of
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",help="Choose browser: chrome or firefox")
+
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption("--browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     browser.implicitly_wait(10)
     yield browser
     print("\nend browser for test..")
@@ -17,3 +31,5 @@ def time_total():
     # для проверки времени использовать https://time.is/ru/
     answer = math.log(int(time.time() + 0.4))
     return answer
+
+
